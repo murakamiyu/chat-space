fd0e3f27c3c37494206381c7a89227f39d7626c3
murakamiyu <42232225+murakamiyu@users.noreply.github.com>
2018-09-05 16:41:40 +0900

    インデント修正

--- app/assets/javascripts/incremental.js
+++ app/assets/javascripts/incremental.js
@@ -1,32 +1,30 @@
 $(function() {
-
-	var search_list = $("#user-search-result")
+  var search_list = $("#user-search-result")
   var member_list = $(".chat-group-user-user")
 
-   function appendUserList(user){
-		var html = `</li><div class="chat-group-user clearfix">
-			  		<p class="chat-group-user__name">${user.name}</p>
-			  		<a class="user-search-add chat-group-user__btn search-add chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
-					</div>
-					`
-		search_list.append(html);
-	}
+  function appendUserList(user){
+    var html = `</li><div class="chat-group-user clearfix">
+	          <p class="chat-group-user__name">${user.name}</p>
+		  <a class="user-search-add chat-group-user__btn search-add chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+		</div>
+	       `
+     search_list.append(html);
+   }
 
+   function appendNoUser(){
+     var html = `</li><div class="chat-group-user clearfix">
+		   <p class="chat-group-user__name">一致するユーザーはいません</p>
+		 </div>
+		`
+     search_list.append(html);
+   }
 
-	function appendNoUser(){
-		var html = `</li><div class="chat-group-user clearfix">
-				       <p class="chat-group-user__name">一致するユーザーはいません</p>
-				     </div>
-		            `
-	    search_list.append(html);
-	}
-
-
-	function appendMember(userIdName){
-    var html =`<div class="chat-group-user clearfix">
+   function appendMember(userIdName){
+     var html =`<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${userIdName.userName}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${userIdName.userId}" data-user-name="${userIdName.userName}">削除</a>
-                  </div>`
+                </div>
+               `
       member_list.append(html);
     }
 
@@ -46,7 +44,6 @@ $(function() {
       if (users.length !== 0) {
           users.forEach(function(user){
           appendUserList(user);
-          console.log(user);
         });
       }
       else {
@@ -58,14 +55,11 @@ $(function() {
     })
   });
   $("#user-search-result").on('click',$(".chat-group-user__btn--add"),function(){
-   
-   var userIdName = $(".user-search-add").data();
-   console.log(userIdName);
- 
-   $(this).remove();
-   appendMember(userIdName);
+     var userIdName = $(".user-search-add").data();
+     $(this).remove();
+     appendMember(userIdName);
   })
   $(".chat-group-user-user").on('click',$("chat-group-user__btn"),function(){
-   $(".chat-group-user").remove();
+    $(".chat-group-user").remove();
   })
 });
