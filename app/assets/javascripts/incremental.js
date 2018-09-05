1d0652518d66c297d32bdcf0f363ad689e467e1b
murakamiyu <42232225+murakamiyu@users.noreply.github.com>
2018-09-05 17:11:57 +0900

    インデント修正

--- app/assets/javascripts/incremental.js
+++ app/assets/javascripts/incremental.js
@@ -31,14 +31,12 @@ $(function() {
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
     console.log(input);
-
     $.ajax({
       type: 'GET',
       url: '/users',
       data: { keyword: input },
       dataType: 'json'
     })
-
     .done(function(users) {
       $("#user-search-result").empty();
       if (users.length !== 0) {
