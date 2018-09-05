4e6af98d8f78b3dd3ce49672a016ed274dfe9f62
murakamiyu <42232225+murakamiyu@users.noreply.github.com>
2018-09-05 17:57:27 +0900

    デバック削除

--- app/assets/javascripts/incremental.js
+++ app/assets/javascripts/incremental.js
@@ -30,7 +30,6 @@ $(function() {
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-    console.log(input);
     $.ajax({
       type: 'GET',
       url: '/users',
