15c2dba4f2421448f7791962268b4f857d08146b
Yusuke Murakami <tropical.inu@gmail.com>
2018-09-06 16:04:59 +0900

    add_some_fixes

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,19 +2,24 @@ $(function(){
   
   function buildHTML(message){
  　 var image = "";
-  	    image = (message.image) ? `<img class="lower-message__image" src="${message.image}">` : "";
-    
+  	    image = (message.image.url) ? `<img class="lower-message__image" src="${message.image.url}">` : "";
+
     var html =`
+                
                 <div class="message">
-	　　　　　　　　　　     <div class="upper-message">
-    	　　　　　　　　        <div class="upper-message__user-name">${message.name}</div>
-	　　　　　　　　　　       <div class="upper-message__date">${message.created_at}</div>
-	　　　　　　　　　　     </div>
-		　　　　　　 <p class="lower-meesage__content" >${message.content}</p>
-	　　　　　　　　　       <div class="lower-message__image">${image}</div>
-    		</div>                
-	       `
+	　　　　　　　　　<div class="upper-message">
+                    <div class="upper-message__user-name">${message.name}</div> 
+                    <div class="upper-message__date">${message.created_at}</div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-meesage__content">${message.content}</p>
+                      ${image}
+                  </div>
+    		        </div>
+                             
+	            `
     return html;
+
   }
 
   $('.new_message').on('submit', function(e){
@@ -33,10 +38,12 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
+      console.log(data)
       $('.messages').append(html)
-      $('.form__message').prop("disabled", false);
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      $('.form__message').val('');
+      $('.form__submit').prop("disabled", false)
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast')
+      // $('.form__message').val('');
+      
     })
     .fail(function(){
       alert('error');
